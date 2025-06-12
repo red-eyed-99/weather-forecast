e0a86acc8ee27f962b2c53c99bbe5fc716edbbad
red-eyed-99 <mgnblack99@gmail.com>
2025-06-12 19:06:21 +0300

    test: add test database migrations

---
+++ src/test/resources/db/migration_test/V1__create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users
+(
+    id       BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(60) NOT NULL,
+
+    CONSTRAINT username_check CHECK (username REGEXP '^[a-zA-Z\d_]{5,20}$'),
+    CONSTRAINT password_encryption_check CHECK (password REGEXP '^\$2[aby]\$\d{2}\$[./A-Za-z0-9]{53}$')
+);
---
+++ src/test/resources/db/migration_test/V4__create_sessions_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE sessions
+(
+    id         CHAR(36) DEFAULT (uuid()) PRIMARY KEY,
+    user_id    BIGINT   NOT NULL,
+    expires_at DATETIME NOT NULL,
+
+    CONSTRAINT id_uuid_check CHECK (
+        id REGEXP '^[a-fA-F\d]{8}-[a-fA-F\d]{4}-[a-fA-F\d]{4}-[a-fA-F\d]{4}-[a-fA-F\d]{12}$'),
+
+    CONSTRAINT expires_at_check CHECK (expires_at > NOW()),
+
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
