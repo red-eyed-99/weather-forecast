390f40b9ae0c49dfe5fb57fdaa12cd8d47d0c15e
red-eyed-99 <mgnblack99@gmail.com>
2025-07-20 13:23:46 +0300

    refactor: add field constants

--- src/main/java/dto/openweather/CoordinatesDTO.java
+++ src/main/java/dto/openweather/CoordinatesDTO.java
@@ -5,4 +5,9 @@ import validation.annotations.ValidLongitude;
 import java.math.BigDecimal;
 
 public record CoordinatesDTO(@ValidLongitude BigDecimal longitude, @ValidLatitude BigDecimal latitude) {
+
+    public static final class Fields {
+        public static final String LONGITUDE = "longitude";
+        public static final String LATITUDE = "latitude";
+    }
 }
