1b3b275536316f63d6f2dc9cdf2bd377d231ad65
red-eyed-99 <mgnblack99@gmail.com>
2025-08-03 14:52:02 +0300

    fix: add constraints in locations table

--- src/test/resources/db/migration_test/V2__create_locations_table.sql
+++ src/test/resources/db/migration_test/V2__create_locations_table.sql
@@ -1,10 +1,13 @@
 CREATE TABLE locations
 (
     id        BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name      VARCHAR(255) NOT NULL,
+    name      VARCHAR(100) NOT NULL UNIQUE,
     latitude  DECIMAL(6,4) NOT NULL,
     longitude DECIMAL(7,4) NOT NULL,
 
+    CONSTRAINT name_check CHECK (
+        name REGEXP '^(?=.*[A-Za-z])[A-Za-z\\d\\s\-,.’''&()/]{1,100}$'
+    ),
     CONSTRAINT latitude_check CHECK(latitude >= -90 AND latitude <= 90),
     CONSTRAINT longitude_check CHECK(longitude >= -180 AND longitude <= 180)
 );
