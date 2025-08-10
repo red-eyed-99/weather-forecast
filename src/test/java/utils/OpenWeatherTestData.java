cbf0322f4c96f336b5ef6824e5c4b4970e7f09ae
red-eyed-99 <mgnblack99@gmail.com>
2025-08-10 11:44:14 +0300

    fix: fix using the same object in tests

--- src/test/java/utils/OpenWeatherTestData.java
+++ src/test/java/utils/OpenWeatherTestData.java
@@ -5,35 +5,21 @@ import dto.openweather.WeatherResponseDTO;
 import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import org.springframework.core.io.ClassPathResource;
-import java.util.HashMap;
-import java.util.Map;
-
-import static utils.LocationTestData.LONDON;
-import static utils.LocationTestData.MOSCOW;
 
 @UtilityClass
 public class OpenWeatherTestData {
 
-    private static final Map<String, WeatherResponseDTO> OPEN_WEATHER_RESPONSES;
+    private static final String SUCCESS_RESPONSE_PATH = "openweather/sucess_response/";
 
-    static {
-        OPEN_WEATHER_RESPONSES = new HashMap<>();
-
-        OPEN_WEATHER_RESPONSES.put(MOSCOW, initWeatherResponseDto(MOSCOW));
-        OPEN_WEATHER_RESPONSES.put(LONDON, initWeatherResponseDto(LONDON));
-    }
+    private static final String JSON_FORMAT = ".json";
 
     @SneakyThrows
-    private static WeatherResponseDTO initWeatherResponseDto(String locationName) {
-        var path = "openweather/sucess_response/" + locationName + ".json";
+    public static WeatherResponseDTO getWeatherResponseDto(String locationName) {
+        var path = SUCCESS_RESPONSE_PATH + locationName + JSON_FORMAT;
 
         var openWeatherResponse = new ClassPathResource(path).getInputStream();
 
         return new ObjectMapper()
                 .readValue(openWeatherResponse, WeatherResponseDTO.class);
-    }
-
-    public static WeatherResponseDTO getWeatherResponseDto(String locationName) {
-        return OPEN_WEATHER_RESPONSES.get(locationName);
     }
 }
