1cfcda035723b9b6e3b5854d7831406c00ea1665
red-eyed-99 <mgnblack99@gmail.com>
2025-10-31 19:56:50 +0300

    feat: add custom pages on 404 and other errors

--- src/main/java/controllers/ExceptionHandlingController.java
+++ src/main/java/controllers/ExceptionHandlingController.java
@@ -2,20 +2,27 @@ package controllers;
 
 import dto.auth.SignInUserDTO;
 import exceptions.BadCredentialsException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static utils.ModelAttributeUtil.ERROR_MESSAGE;
 import static utils.ModelAttributeUtil.USER;
+import static utils.PagesUtil.ERROR_404;
+import static utils.PagesUtil.ERROR_500;
 import static utils.PagesUtil.SEARCH_LOCATIONS;
 import static utils.PagesUtil.SIGN_IN;
 
 @ControllerAdvice
+@Slf4j
 public class ExceptionHandlingController {
 
     @ExceptionHandler(BadCredentialsException.class)
@@ -39,5 +46,18 @@ public class ExceptionHandlingController {
         model.addAttribute(ERROR_MESSAGE, message);
 
         return SEARCH_LOCATIONS;
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(NOT_FOUND)
+    public String handleNoHandlerFoundException() {
+        return ERROR_404;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    public String handleException(Exception exception) {
+        log.error(exception.getMessage(), exception);
+        return ERROR_500;
     }
 }
--- src/main/java/utils/PagesUtil.java
+++ src/main/java/utils/PagesUtil.java
@@ -15,4 +15,7 @@ public class PagesUtil {
     public static final String SEARCH_LOCATIONS = "search-locations/search-locations";
 
     public static final String REDIRECT_HOME = REDIRECT_URL_PREFIX + "/";
+
+    public static final String ERROR_500 = "errors/500.html";
+    public static final String ERROR_404 = "errors/404.html";
 }
